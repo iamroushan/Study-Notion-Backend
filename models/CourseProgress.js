9d8537109eaee4b564c7a0b4bda568a7a8964ef6
iamroushan <roushanrnd@gmail.com>
2024-02-05 22:54:04 +0530

    added the various models

---
+++ models/CourseProgress.js
@@ -0,0 +1,17 @@
+const mongoose= require("mongoose")
+
+const courseProgressSchema= new mongoose.Schema({
+    
+    courseID:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+    },
+    completedVideos:[
+        {
+            types:  mongoose.Schema.Types.ObjectId,  // Array of video IDs that have been completed
+            ref: "SubSection"
+        }
+    ]
+})
+
+module.exports= mongoose.model("CourseProgress", courseProgressSchema)
