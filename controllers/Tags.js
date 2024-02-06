542c9217497a6824255353bbe8c78a2767b2bd3c
iamroushan <roushanrnd@gmail.com>
2024-02-07 00:01:15 +0530

    added the tags handler controller

---
+++ controllers/Tags.js
@@ -0,0 +1,55 @@
+const tags = require("../models/tags")
+const Tag= require("../models/tags")
+
+// create tag ka handler function
+exports.createTag= async(req,res)=>{
+    try {
+        // fetch data   
+        const {name, description}= req.body
+
+        // validation
+        if(!name || !description){
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required"
+            })
+        }
+
+        // create entry in db
+        const tagDetails = await Tag.create({
+            name: name,
+            description: description
+        }) 
+        console.log(tagDetails);
+
+        // return response
+        return res.status(200).json({
+            success: true,
+            message: "Tag created successfully"
+        })
+    }
+    catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+// getAllTags handler function
+exports.showAllTags = async (req,res)=>{
+    try {
+        const allTags = await Tag.find({},{name: true, description: true})
+        res.status(200).json({
+            succes: true,
+            message: "All tags return successfully",
+            allTags
+        })
+    } 
+    catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
